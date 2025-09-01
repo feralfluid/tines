d2205a58733d39c71042cc94a02c2fbf24f9a62e
Sophie Coyote <git@flotts.io>
2025-08-31 19:22:33 -0700

    give in to the favicon hellscape we live in

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,48 @@ export default defineConfig({
 			],
 			pagefind: false,
 			tableOfContents: false,
+
+			head: [
+		    {
+		      tag: 'link',
+		      attrs: {
+		        rel: 'icon',
+		        type: 'image/png',
+		        href: '/favicon/favicon-96x96.png',
+		        sizes: '96x96',
+		      },
+		    },
+		    {
+		      tag: 'link',
+		      attrs: {
+		        rel: 'icon',
+		        type: 'image/svg+xml',
+		        href: '/favicon/favicon.svg',
+		      },
+		    },
+		    {
+		      tag: 'link',
+		      attrs: {
+		        rel: 'shortcut icon',
+		        href: '/favicon/favicon.ico',
+		      },
+		    },
+		    {
+		      tag: 'link',
+		      attrs: {
+		        rel: 'apple-touch-icon',
+		        href: '/favicon/apple-touch-icon.png',
+		        sizes: '180x180',
+		      },
+		    },
+		    {
+		      tag: 'link',
+		      attrs: {
+		        rel: 'manifest',
+		        href: '/favicon/site.webmanifest',
+		      },
+		    },
+		  ],
 		}),
 	],
 	site: 'https://packofpuppies.com',
